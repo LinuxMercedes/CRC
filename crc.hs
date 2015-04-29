1fa44a1a8de0c19aa301386398ec763ac69c9388
LinuxMercedes <LinuxMercedes@gmail.com>
2015-04-29 16:51:27 -0500

    Convert tabs to spaces

--- crc.hs
+++ crc.hs
@@ -8,28 +8,28 @@ data Bit = O | I deriving (Eq, Show)
 trunc :: [Bit] -> [Bit]
 trunc [] = []
 trunc (x:xs) 
-	| x == O = trunc xs
-	| otherwise = x:xs
+  | x == O = trunc xs
+  | otherwise = x:xs
 
 sub :: [Bit] -> [Bit] -> [Bit]
 sub x y = zipWith _sub x y ++ drop (length y) x
-	where
-		_sub :: Bit -> Bit -> Bit
-		_sub O O = O
-		_sub I O = I
-		_sub O I = I
-		_sub I I = O
+  where
+    _sub :: Bit -> Bit -> Bit
+    _sub O O = O
+    _sub I O = I
+    _sub O I = I
+    _sub I I = O
 
 remainder :: [Bit] -> [Bit] -> [Bit]
 remainder dividend divisor
-	| length divisor > length dividend = dividend
-	| head dividend == O = remainder(tail dividend) divisor
-	| otherwise = remainder (tail $ sub dividend divisor) divisor
+  | length divisor > length dividend = dividend
+  | head dividend == O = remainder(tail dividend) divisor
+  | otherwise = remainder (tail $ sub dividend divisor) divisor
 
 pad :: Int -> [Bit] -> [Bit]
 pad n xs
-	| length xs >= n = xs
-	| otherwise = pad n $ xs ++ [O]
+  | length xs >= n = xs
+  | otherwise = pad n $ xs ++ [O]
 
 -- Generic CRC stuff
 crc :: [Bit] -> [Bit] -> [Bit]
@@ -37,8 +37,8 @@ crc val polynomial = val ++ remainder (pad (length polynomial + length val - 1) 
 
 crccheck :: [Bit] -> [Bit] -> Maybe [Bit]
 crccheck val polynomial 
-	| 0 == (length . trunc $ remainder val polynomial) = Just $ take (length val - length polynomial + 1) val
-	| otherwise = Nothing
+  | 0 == (length . trunc $ remainder val polynomial) = Just $ take (length val - length polynomial + 1) val
+  | otherwise = Nothing
 
 -- 100000100110000010001110110110111
 -- IEEE 802.3 generator polynomial
